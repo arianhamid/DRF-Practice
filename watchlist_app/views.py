4b856b72955e1af96cc847f89fec99256a27a37e
Hamid <hamidarianyegane@gmail.com>
2023-10-04 00:46:08 +0330

    Django Rest IMDB API

---
+++ watchlist_app/views.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+from watchlist_app.models import Movie
+# Create your views here.
+
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {'movies': list(movies.values())}
+    return JsonResponse(data)
