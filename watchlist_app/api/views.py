08ddbe2fbc066939e51a16e5d5870d66ea8efea3
Hamid <hamidarianyegane@gmail.com>
2023-10-08 23:52:25 +0330

    status codes added

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -26,7 +26,11 @@ def movie_list(request):
 def movie_details(request, pk):
 
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'Error': 'Movie does not Found'}, status=status.HTTP_404_NOT_FOUND)
+
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
 
@@ -37,7 +41,7 @@ def movie_details(request, pk):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == 'DELETE':
         movie = Movie.objects.get(pk=pk)
